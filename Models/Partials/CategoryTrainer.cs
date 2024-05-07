9531209b5dfef81674a905ae0271e1e70744757e
RuslanRinatovich <safiullin@yandexlyceum.ru>
2024-05-07 16:14:52 +0300

    Добавить диплом

---
+++ Models/Partials/CategoryTrainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfAssortmentCheck.Models
+{
+    public partial class CategoryTrainer
+    {
+        public Visibility GetVisibility
+        {
+            get
+            {
+                if (Manager.CurrentUser == null)
+                    return Visibility.Collapsed;
+                if (Convert.ToInt32(Manager.CurrentUser.Role) != 0)
+                    return Visibility.Collapsed;
+                return Visibility.Visible;
+            }
+        }
+    }
+}
